36327aef5e1e533b66eddcaa6fc063b5792c5be2
Mihkel Jäe <31515689+jaaae@users.noreply.github.com>
2018-06-07 15:48:02 +0300

    main page prototype

---
+++ main.js
@@ -0,0 +1,59 @@
+const MainApp = function () {
+  if (MainApp.instance) {
+    return MainApp.instance
+  }
+  MainApp.instance = this
+
+  this.routes = MainApp.routes
+  this.currentRoute = null
+
+  this.init()
+}
+
+MainApp.routes = {
+  'home-view': {
+    'render': function () {
+    }
+  },
+  'app-view': {
+    'render': function () {
+    }
+  }
+}
+
+MainApp.prototype = {
+  init: function () {
+
+    window.addEventListener('hashchange', this.routeChange.bind(this))
+
+    if (!window.location.hash) {
+      window.location.hash = 'home-view'
+    } else {
+      this.routeChange()
+    }
+  },
+
+  routeChange: function (event) {
+    this.currentRoute = location.hash.slice(1)
+    if (this.routes[this.currentRoute]) {
+      this.updateMenu()
+
+      this.routes[this.currentRoute].render()
+    } else {
+      /// 404 - ei olnud
+    }
+  },
+  
+  updateMenu: function () {
+    // http://stackoverflow.com/questions/195951/change-an-elements-class-with-javascript
+    document.querySelector('.active-menu').className = document.querySelector('.active-menu').className.replace('active-menu', '')
+    document.querySelector('.' + this.currentRoute).className += ' active-menu'
+  }
+
+}//main
+
+window.onload = function () {
+  const app = new MainApp()
+  window.app = app
+}
+
---
+++ main.php
@@ -0,0 +1,42 @@
+<?php
+  //require("functions.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Main</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" media="screen" href="mainpage.css" />
+  <script src="main.js" charset="utf-8"></script>
+<body>
+  
+  <!-- Menu -->
+  <nav class="menu">
+    <ul class="menu-list">
+      <li class="menu-item"><a href="#home-view" class="menu-link home-view active-menu">Avaleht</a></li>
+      <li class="menu-item"><a href="#app-view" class="menu-link app-view">Päevik</a></li>
+    </ul>
+  </nav>
+
+  <main role="main">
+
+    <!-- App page -->
+    <section id="app-view">
+        <h1>Päevik</h1>
+        <p>Siin on sinu päevik</p>
+        
+    </section>
+
+    <!-- Home -->
+    <section id="home-view">
+        <h2>Mängusõltuvuse eneseseire keskkond</h2>
+        <h1>Avaleht</h1>
+        <p>Siin on sinu avaleht!</p>
+    </section>
+  
+  </main>
+
+</body>
+</html>
