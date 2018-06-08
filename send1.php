8be52ad28b45ee2087cb28d28bdee73bb356b02d
Tanel Kuklane <kukltane@greeny.cs.tlu.ee>
2018-06-08 13:46:04 +0300

    changed some directories

--- send1.php
+++ send1.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	require("../../config.php");
+	require("../../../../config.php");
 	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 	$stmt = $mysqli->prepare("INSERT INTO diary (answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?)");
 	$stmt->bind_param("iiii",$_REQUEST["a1"], $_REQUEST["a2"],$_REQUEST["a3"],$_REQUEST["a4"]);
