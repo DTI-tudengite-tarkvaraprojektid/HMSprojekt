2dc4bb2e7e2e130421b6b0395aa0e6d7ecb57f78
Tanel Kuklane <kukltane@greeny.cs.tlu.ee>
2018-06-08 23:16:45 +0300

    id database add

--- send1.php
+++ send1.php
@@ -3,7 +3,7 @@
 	require("../../../../config.php");
 	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 	$stmt = $mysqli->prepare("INSERT INTO diary (id, answer1, answer2, answer3, answer4) VALUES (?, ?, ?, ?, ?)");
-	$stmt->bind_param("iiiii",$_SESSION['userid'], $_REQUEST["a1"], $_REQUEST["a2"],$_REQUEST["a3"],$_REQUEST["a4"]);
+	$stmt->bind_param("iiiii", $_SESSION['userid'], $_REQUEST["a1"], $_REQUEST["a2"],$_REQUEST["a3"],$_REQUEST["a4"]);
 	$stmt->execute();
 	$stmt->close();
 	$mysqli->close();
