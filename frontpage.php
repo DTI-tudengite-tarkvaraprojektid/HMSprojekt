b0a06b6a8a4c1e1243e8d241d265df319bd780e2
Tanel Kuklane <kukltane@greeny.cs.tlu.ee>
2018-06-08 09:26:18 +0300

    tree

---
+++ frontpage.php
@@ -0,0 +1,188 @@
+<?php
+require("../../config.php");
+$reasons = array("Töökohustused", "Suhted lähedastega", "Suhted sõpradega", "Midagi muud");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA_Compatible" content="ie=edge">
+	<title>Küsimuste puu</title>
+	<script src="tree.js"></script>
+	<link rel="stylesheet" type="text/css" href="disain.css">
+	
+</head>
+	
+<body>
+
+<div id="sections">
+
+	<div id="section1">
+		<p id="question1"> 1. Mil määral ma tajun, et mu meelistegevuse sooritamine arvutis on kontrolli all? </p>
+
+		<input type="radio" id="answer11" name="answer1" value="0">
+
+		<p id="option11"> Üldse mitte </p>
+
+		<input type="radio" id="answer12" name="answer1" value="1">
+
+		<p id="option12"> Vähe </p>
+
+		<input type="radio" id="answer13" name="answer1" value="2">
+
+		<p id="option13"> Keskmiselt </p>
+
+		<input type="radio" id="answer14" name="answer1" value="3"> 
+
+		<p id="option14"> Palju </p>
+
+		<input type="radio" id="answer15" name="answer1" value="4">
+
+		<p id="option15"> Täiesti </p>
+		
+		<button id="button1" type="button" onclick="button1()">Fikseeri vastus</button>
+		
+	</div>
+	
+	<div id="start">
+
+	<div id="section2">
+		<p id="question2"> 2. Kui tugevalt hindan meelistegevust sooritada täna? </p>
+
+		<input type="radio" id="answer21" name="answer2" value="0">
+
+		<p id="option21"> Olematu </p>
+
+		<input type="radio" id="answer22" name="answer2" value="1">
+
+		<p id="option22"> Nõrk </p>
+
+		<input type="radio" id="answer23" name="answer2" value="2">
+
+		<p id="option23"> Tavaline </p>
+
+		<input type="radio" id="answer24" name="answer2" value="3"> 
+
+		<p id="option24"> Kõrge </p>
+
+		<input type="radio" id="answer25" name="answer2" value="4">
+
+		<p id="option25"> Väga kõrge </p>
+
+		<button id="button2" type="button" onclick="button2()">Fikseeri vastus</button> 
+	</div>
+
+	<div id="section3">
+		<p id="question3"> 3. Kui tõenäoliselt ma suudan soovile meelistegevust (liigselt) sooritada vastu seista? </p>
+			
+		<input type="radio" id="answer31" name="answer3" value="0">
+		
+		<p id="option31"> Üldse mitte </p>
+		
+		<input type="radio" id="answer32" name="answer3" value="1">
+		
+		<p id="option32"> Vähe </p>
+		
+		<input type="radio" id="answer33" name="answer3" value="2">
+		
+		<p id="option33"> Keskmiselt </p>
+		
+		<input type="radio" id="answer34" name="answer3" value="3"> 
+		
+		<p id="option34"> Palju </p>
+		
+		<input type="radio" id="answer35" name="answer3" value="4">
+		
+		<p id="option35"> Täiesti </p>
+		
+		<button id="button3" type="button" onclick="button3()">Fikseeri vastus</button>
+	</div>
+
+	<div id="section4">
+		<p id="question4"> 4. Kas ma täna sooritasin oma meelistegevust? </p>
+			
+		<input type="radio" id="answer41" name="answer4"  value="0">
+			
+		<p id="option41"> Jah </p>
+			
+		<input type="radio" id="answer42" name="answer4" value="1">
+			
+		<p id="option42"> Ei </p>
+			
+		<button id="button4" type="button" onclick="button4()">Fikseeri vastus</button>
+	</div>
+	
+	<p id="end1"> Sa olid täna väga tubli, jätka samas vaimus!!! </p>
+
+	<div id="section5">
+		<p id="question5"> 5. Kui palju aega (tunde ja minuteid) ma seda tegevust sooritades veetsin? </p>
+	 
+		<p id="answer5start"> Ma kulutasin selle tegevuse peale aega </p>
+		
+		<input id="hours" type="number" min="0" max="24" value="0">
+	   
+		<p id="answer5center"> tundi ja </p>
+			
+		<input id="minutes" type="number" min="0" max="59" value="0">
+		
+		<p  id="answer5end"> minutit. </p>
+		
+		<button id="button5" type="button" onclick="button5()">Fikseeri vastus</button>
+	</div>
+	
+	<div id="section61">
+		<p id="question6"> 6. Kas mul jäi selle tegevuse tõttu muud tegevused sooritamata või ma ei jõudnud neid õigeaegselt valmis? 
+			Kui jah, siis millised muud tegevused seetõttu kannatasid? </p>
+
+		<input type="radio" id="answer61" name="answer6"  value="0">
+		
+		<p id="option61"> Jah </p>
+		
+		<input type="radio" id="answer62" name="answer6" value="1">
+		
+		<p id="option62"> Ei </p>
+		
+		<button id="button6" type="button" onclick="button61()">Fikseeri vastus</button>
+	</div>
+		
+	<div id="section62">
+		<p id="what"> Millised tegevused kannatasid? </p>
+		
+		<?php 
+			echo "<select id='reasons'>";
+			for ($i = 0; $i < count($reasons); $i++) {
+				echo '<option value="' .$reasons[$i] .'">'.$reasons[$i] ."</option>";
+			}
+			echo "</select>";
+		?>
+			
+		<button id="button8" type="button" onclick="button62()">Fikseeri vastus</button>
+	</div>
+
+	<div id="section63">
+		<p id="whatnew"> Milline muu tegevus selle tõttu kannatas? </p>
+		
+		<input id="newreason" type="text" value="" size="43px">
+		
+		<button id="button9" type="button" onclick="button63()">Fikseeri vastus</button>
+	</div>
+
+	<div id="section7">
+		<p id="question7"> 7. Kirjelda oma mõtteid ja tundeid ning nende muutumist päeva jooksul. </p>
+
+		<input id="answer7" type="text" size="70px" value="">
+		
+		<button id="button7" type="button" onclick="button7()">Fikseeri vastus</button>
+	</div>
+	
+	<p id="end2"> Sa andsid küll täna oma nõrkusele järele, aga püüa vähemalt homme tublim olla!!! </p>
+
+	</div>
+	
+</div>
+
+</body>
+
+</html>
--- tree.js
+++ tree.js
@@ -1,183 +1,129 @@
 window.onload = function () {
-    document.getElementById("question2").style.visibility = "hidden";
-    document.getElementById("question3").style.visibility = "hidden";
-    document.getElementById("question4").style.visibility = "hidden";
-    document.getElementById("question5").style.visibility = "hidden";
-    document.getElementById("question6").style.visibility = "hidden";
-    document.getElementById("question7").style.visibility = "hidden";
-    document.getElementById("answer21").style.visibility = "hidden";
-    document.getElementById("answer22").style.visibility = "hidden";
-    document.getElementById("answer23").style.visibility = "hidden";
-    document.getElementById("answer24").style.visibility = "hidden";
-    document.getElementById("answer25").style.visibility = "hidden";
-    document.getElementById("option21").style.visibility = "hidden";
-    document.getElementById("option22").style.visibility = "hidden";
-    document.getElementById("option23").style.visibility = "hidden";
-    document.getElementById("option24").style.visibility = "hidden";
-    document.getElementById("option25").style.visibility = "hidden";
-    document.getElementById("answer31").style.visibility = "hidden";
-    document.getElementById("answer32").style.visibility = "hidden";
-    document.getElementById("answer33").style.visibility = "hidden";
-    document.getElementById("answer34").style.visibility = "hidden";
-    document.getElementById("answer35").style.visibility = "hidden";
-    document.getElementById("option31").style.visibility = "hidden";
-    document.getElementById("option32").style.visibility = "hidden";
-    document.getElementById("option33").style.visibility = "hidden";
-    document.getElementById("option34").style.visibility = "hidden";
-    document.getElementById("option35").style.visibility = "hidden";
-    document.getElementById("answer41").style.visibility = "hidden";
-    document.getElementById("answer42").style.visibility = "hidden";
-    document.getElementById("answer5start").style.visibility = "hidden";
-    document.getElementById("answer5center").style.visibility = "hidden";
-    document.getElementById("answer5end").style.visibility = "hidden";
-    document.getElementById("answer61").style.visibility = "hidden";
-    document.getElementById("answer62").style.visibility = "hidden";
-    document.getElementById("answer7").style.visibility = "hidden";
-    document.getElementById("hours").style.visibility = "hidden";
-    document.getElementById("minutes").style.visibility = "hidden";
-    document.getElementById("option41").style.visibility = "hidden";
-    document.getElementById("option42").style.visibility = "hidden";
-    document.getElementById("option61").style.visibility = "hidden";
-    document.getElementById("option41").style.visibility = "hidden";
-    document.getElementById("option62").style.visibility = "hidden";
-    document.getElementById("option42").style.visibility = "hidden";
-    document.getElementById("button2").style.visibility = "hidden";
-    document.getElementById("button3").style.visibility = "hidden";
-    document.getElementById("button4").style.visibility = "hidden";
-    document.getElementById("button5").style.visibility = "hidden";
-    document.getElementById("button6").style.visibility = "hidden";
-    document.getElementById("button7").style.visibility = "hidden";
-    document.getElementById("button8").style.visibility = "hidden";
-    document.getElementById("button9").style.visibility = "hidden";
-    document.getElementById("end1").style.visibility = "hidden";
-    document.getElementById("end2").style.visibility = "hidden";
-    document.getElementById("reasons").style.visibility = "hidden";
-    document.getElementById("what").style.visibility = "hidden";
-    document.getElementById("whatnew").style.visibility = "hidden";
-    document.getElementById("newreason").style.visibility = "hidden";
+	document.getElementById("start").style.visibility = "hidden";
 }
 
 function button1() {
-    if(document.getElementById("answer11").checked == true ||
-        document.getElementById("answer12").checked == true ||
-        document.getElementById("answer13").checked == true ||
-        document.getElementById("answer14").checked == true ||
-        document.getElementById("answer15").checked == true ){
-            document.getElementById("question2").style.visibility = "visible";
-            document.getElementById("answer21").style.visibility = "visible";
-            document.getElementById("answer22").style.visibility = "visible";
-            document.getElementById("answer23").style.visibility = "visible";
-            document.getElementById("answer24").style.visibility = "visible";
-            document.getElementById("answer25").style.visibility = "visible";
-            document.getElementById("option21").style.visibility = "visible";
-            document.getElementById("option22").style.visibility = "visible";
-            document.getElementById("option23").style.visibility = "visible";
-            document.getElementById("option24").style.visibility = "visible";
-            document.getElementById("option25").style.visibility = "visible";
-            document.getElementById("button2").style.visibility = "visible";
-    }    
+	if(document.getElementById("answer11").checked == true){
+		document.getElementById("section2").style.visibility = "visible";
+		var Answer1 = "1";
+	}
+	if(document.getElementById("answer12").checked == true){
+		document.getElementById("section2").style.visibility = "visible";
+		var Answer1 = "2";
+	}
+	if(document.getElementById("answer13").checked == true){
+		document.getElementById("section2").style.visibility = "visible";
+		var Answer1 = "3";
+	}
+	if(document.getElementById("answer14").checked == true){
+		document.getElementById("section2").style.visibility = "visible";
+		var Answer1 = "4";
+	}
+	if(document.getElementById("answer15").checked == true){
+		document.getElementById("section2").style.visibility = "visible";
+		var Answer1 = "5";
+	}
 }
 
 function button2() {
-    if(document.getElementById("answer21").checked == true ||
-        document.getElementById("answer22").checked == true ||
-        document.getElementById("answer23").checked == true ||
-        document.getElementById("answer24").checked == true ||
-        document.getElementById("answer25").checked == true ){
-            document.getElementById("question3").style.visibility = "visible";
-            document.getElementById("answer31").style.visibility = "visible";
-            document.getElementById("answer32").style.visibility = "visible";
-            document.getElementById("answer33").style.visibility = "visible";
-            document.getElementById("answer34").style.visibility = "visible";
-            document.getElementById("answer35").style.visibility = "visible";
-            document.getElementById("option31").style.visibility = "visible";
-            document.getElementById("option32").style.visibility = "visible";
-            document.getElementById("option33").style.visibility = "visible";
-            document.getElementById("option34").style.visibility = "visible";
-            document.getElementById("option35").style.visibility = "visible";
-            document.getElementById("button3").style.visibility = "visible";
-    }
+	if(document.getElementById("answer21").checked == true){
+		document.getElementById("section3").style.visibility = "visible";
+		var Answer2 = "0";
+	}
+	if(document.getElementById("answer22").checked == true){
+		document.getElementById("section3").style.visibility = "visible";
+		var Answer2 = "1";
+	}
+	if(document.getElementById("answer23").checked == true){
+		document.getElementById("section3").style.visibility = "visible";
+		var Answer2 = "2";
+	}
+	if(document.getElementById("answer24").checked == true){
+		document.getElementById("section3").style.visibility = "visible";
+		var Answer2 = "3";
+	}
+	if(document.getElementById("answer25").checked == true){
+		document.getElementById("section3").style.visibility = "visible";
+		var Answer2 = "4";
+	}
 }
 
 function button3() {
-    if(document.getElementById("answer31").checked == true ||
-        document.getElementById("answer32").checked == true ||
-        document.getElementById("answer33").checked == true ||
-        document.getElementById("answer34").checked == true ||
-        document.getElementById("answer35").checked == true ){
-            document.getElementById("question4").style.visibility = "visible";
-            document.getElementById("answer41").style.visibility = "visible";
-            document.getElementById("answer42").style.visibility = "visible";
-            document.getElementById("option41").style.visibility = "visible";
-            document.getElementById("option42").style.visibility = "visible";
-            document.getElementById("button4").style.visibility = "visible";
-
-        }
+	if(document.getElementById("answer31").checked == true){
+		document.getElementById("section4").style.visibility = "visible";
+		var Answer3 = "0";
+	}
+	if(document.getElementById("answer32").checked == true){
+		document.getElementById("section4").style.visibility = "visible";
+		var Answer3 = "1";
+	}
+	if(document.getElementById("answer33").checked == true){
+		document.getElementById("section4").style.visibility = "visible";
+		var Answer3 = "2";
+	}
+	if(document.getElementById("answer34").checked == true){
+		document.getElementById("section4").style.visibility = "visible";
+		var Answer3 = "3";
+	}
+	if(document.getElementById("answer35").checked == true){
+		document.getElementById("section4").style.visibility = "visible";
+		var Answer3 = "4";
+	}
 }
 
 function button4() {
     if(document.getElementById("answer41").checked == true){
-        document.getElementById("question5").style.visibility = "visible";
-        document.getElementById("answer5start").style.visibility = "visible";
-        document.getElementById("answer5center").style.visibility = "visible";
-        document.getElementById("answer5end").style.visibility = "visible";
-        document.getElementById("hours").style.visibility = "visible";
-        document.getElementById("minutes").style.visibility = "visible";
-        document.getElementById("button5").style.visibility = "visible";
+        document.getElementById("section5").style.visibility = "visible";
+		document.getElementById("end1").style.visibility = "hidden";
+		var Answer4 = "0";
     }
     if(document.getElementById("answer42").checked == true){
         document.getElementById("end1").style.visibility = "visible"; 
+		var Answer4 = "1";
     }
 }
 
 function button5() {
     if(document.getElementById("hours").value!=0 || document.getElementById("minutes").value!=0){
-        document.getElementById("question6").style.visibility = "visible";
-        document.getElementById("answer61").style.visibility = "visible";
-        document.getElementById("answer62").style.visibility = "visible";
-        document.getElementById("option61").style.visibility = "visible";
-        document.getElementById("option62").style.visibility = "visible";
-        document.getElementById("button6").style.visibility = "visible";
+        document.getElementById("section61").style.visibility = "visible";
+		var Answer5 = document.getElementById("hours").value*60 + parseInt(document.getElementById("minutes").value);
     }
 }
 
-function button6() {
+function button61() {
     if(document.getElementById("answer61").checked == true){
-        document.getElementById("what").style.visibility = "visible";
-        document.getElementById("reasons").style.visibility = "visible";
-        document.getElementById("button8").style.visibility = "visible";
+        document.getElementById("section62").style.visibility = "visible";
+		var Answer61 = "0";
     }
     if(document.getElementById("answer62").checked == true){
-        document.getElementById("question7").style.visibility = "visible";
-        document.getElementById("answer7").style.visibility = "visible";
-        document.getElementById("button7").style.visibility = "visible";
+        document.getElementById("section7").style.visibility = "visible";
+		var Answer61 = "1";
+    }
+}
+
+function button62() {
+    if(document.getElementById("reasons").value == "Midagi muud"){
+        document.getElementById("section63").style.visibility = "visible";
+		var Answer62 = document.getElementById("reasons").value;
+    }else{
+        document.getElementById("section7").style.visibility = "visible";
+		var Answer62 = document.getElementById("reasons").value;
+    }
+}
+
+function button63() {
+    if(document.getElementById("newreason").value != ""){
+        document.getElementById("section7").style.visibility = "visible";
+		var Answer63 = document.getElementById("newreason").value;
     }
 }
 
 function button7() {
     if(document.getElementById("answer7").value != ""){
         document.getElementById("end2").style.visibility = "visible";
+		var Answer7 = document.getElementById("answer7").value;
     }
 }
 
-function button8() {
-    if(document.getElementById("reasons").value == "Midagi muud"){
-        document.getElementById("whatnew").style.visibility = "visible";
-        document.getElementById("newreason").style.visibility = "visible";
-        document.getElementById("button9").style.visibility = "visible";
-    }else{
-        document.getElementById("question7").style.visibility = "visible";
-        document.getElementById("answer7").style.visibility = "visible";
-        document.getElementById("button7").style.visibility = "visible";
-    }
-}
-
-function button9() {
-    if(document.getElementById("newreason").value != ""){
-        document.getElementById("question7").style.visibility = "visible";
-        document.getElementById("answer7").style.visibility = "visible";
-        document.getElementById("button7").style.visibility = "visible";
-    }
-}
 
 
