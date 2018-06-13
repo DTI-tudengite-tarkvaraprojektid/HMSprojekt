ad0286d833d828295ffb0f09367f5272b1e7bf5b
Tanel Kuklane <kukltane@greeny.cs.tlu.ee>
2018-06-13 11:15:42 +0300

    average, minimum and maximum calculate

--- statistics.js
+++ statistics.js
@@ -1,6 +1,14 @@
-document.getElementById("diaryEntry").addEventListener("click", diaryEntry);
+function starter(){
+  document.getElementById("diaryEntry").addEventListener("click", diaryEntry);
+  document.getElementById("entrys1").addEventListener("click", entrys);
+  document.getElementById("usersCount1").addEventListener("click", usersCount);
+  document.getElementById("sendAverage").addEventListener("click", sendAverage);
+  document.getElementById("sendMinimum").addEventListener("click", sendMinimum);
+  document.getElementById("sendMaximum").addEventListener("click", sendMaximum);
+}
 
-var id = null, date =null;
+var id = null, date = null, idchoice2 = null, answerNr1 = null;
+var idchoice3 = null, answerNr2 = null, idchoice4 = null, answerNr3 = null;
 
 function entrys() {
 	var xhr=new XMLHttpRequest();
@@ -38,6 +46,51 @@ function diaryEntry() {
 		if(xhr.readyState==4){
 			console.log(xhr.responseText);
 			document.getElementById("diaryAnswers").innerHTML = this.responseText;
+		}
+	}
+	xhr.send();
+}
+
+function sendAverage() {
+	id = document.getElementById("idchoice2").value;
+	answerNr1 = document.getElementById("answerNr1").value;
+	var xhr=new XMLHttpRequest();
+	var aadress="average.php?a1="+id+"&a2="+answerNr1;
+	xhr.open("GET", aadress, true);	
+	xhr.onreadystatechange=function(){
+		if(xhr.readyState==4){
+			console.log(xhr.responseText);
+			document.getElementById("average").innerHTML = this.responseText;
+		}
+	}
+	xhr.send();
+}
+
+function sendMinimum() {
+	id = document.getElementById("idchoice3").value;
+	answerNr2 = document.getElementById("answerNr2").value;
+	var xhr=new XMLHttpRequest();
+	var aadress="minimum.php?a1="+id+"&a2="+answerNr2;
+	xhr.open("GET", aadress, true);	
+	xhr.onreadystatechange=function(){
+		if(xhr.readyState==4){
+			console.log(xhr.responseText);
+			document.getElementById("minimum").innerHTML = this.responseText;
+		}
+	}
+	xhr.send();
+}
+
+function sendMaximum() {
+	id = document.getElementById("idchoice4").value;
+	answerNr3 = document.getElementById("answerNr3").value;
+	var xhr=new XMLHttpRequest();
+	var aadress="maximum.php?a1="+id+"&a2="+answerNr3;
+	xhr.open("GET", aadress, true);	
+	xhr.onreadystatechange=function(){
+		if(xhr.readyState==4){
+			console.log(xhr.responseText);
+			document.getElementById("maximum").innerHTML = this.responseText;
 		}
 	}
 	xhr.send();
--- statistics.php
+++ statistics.php
@@ -8,13 +8,12 @@ require("../../../../config.php");
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" type="text/css" href="style.css">
 <script src="statistics.js" defer></script>
-
 <!--<base target="_blank">-->
 <title>HMS</title>
 </head>
-<body>
+<body onload="starter()">
 
-<p> Sissekannete arv lehel: </p>
+<p> Sissekannete arv kokku: </p>
 <p id="entrys"></p>
 
 <button id="entrys1" type="button">Uuenda</button>
@@ -45,11 +44,11 @@ Vali kuupäev:
 
 <button id="diaryEntry" type="button">Uuenda</button>
 
-<p> Suuruste (keskmine, miinimum ja maksimum) arvutamine: </p>
+<p> Keskmise arvutamine: </p>
 
 Vali küsimus:
 
-<select id="answerNr">
+<select id="answerNr1">
 	<option value="0">1. Mil määral ma tajun, et mu meelistegevuse sooritamine arvutis on kontrolli all?</option>
 	<option value="1">2. Kui tugevalt hindan meelistegevust sooritada täna?</option>
 	<option value="2">3. Kui tõenäoliselt ma suudan soovile meelistegevust (liigselt) sooritada vastu seista?</option>
@@ -62,7 +61,57 @@ Vali id:
 	<option value="21">21</option>
 	<option value="22">22</option>
 	<option value="33">33</option>
-</select>
+</select><br><br>
+
+<p id="average"></p>
+
+<button id="sendAverage" type="button">Uuenda</button>
+
+<p> Miinimumi arvutamine: </p>
+
+Vali küsimus:
+
+<select id="answerNr2">
+	<option value="0">1. Mil määral ma tajun, et mu meelistegevuse sooritamine arvutis on kontrolli all?</option>
+	<option value="1">2. Kui tugevalt hindan meelistegevust sooritada täna?</option>
+	<option value="2">3. Kui tõenäoliselt ma suudan soovile meelistegevust (liigselt) sooritada vastu seista?</option>
+</select><br>
+
+Vali id:
+
+<select id="idchoice3">
+	<option value="20">20</option>
+	<option value="21">21</option>
+	<option value="22">22</option>
+	<option value="33">33</option>
+</select><br><br>
+
+<p id="minimum"></p>
+
+<button id="sendMinimum" type="button">Uuenda</button>
+
+<p> Maksimumi arvutamine: </p>
+
+Vali küsimus:
+
+<select id="answerNr3">
+	<option value="0">1. Mil määral ma tajun, et mu meelistegevuse sooritamine arvutis on kontrolli all?</option>
+	<option value="1">2. Kui tugevalt hindan meelistegevust sooritada täna?</option>
+	<option value="2">3. Kui tõenäoliselt ma suudan soovile meelistegevust (liigselt) sooritada vastu seista?</option>
+</select><br>
+
+Vali id:
+
+<select id="idchoice4">
+	<option value="20">20</option>
+	<option value="21">21</option>
+	<option value="22">22</option>
+	<option value="33">33</option>
+</select><br><br>
+
+<p id="maximum"></p>
+
+<button id="sendMaximum" type="button">Uuenda</button>
 
 </body>
 </html>
