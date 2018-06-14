644ffc2e30d55aa944eb6effff08084bcd18fa60
Tanel Kuklane <t1a2n3@tlu.ee>
2018-06-14 11:41:31 +0300

    frequencys table

--- frequencys.php
+++ frequencys.php
@@ -6,30 +6,35 @@
 	$answerNr4 = $_REQUEST["a2"];
 
 	if($answerNr4==0){
-		$sql = "SELECT answer4, ROUND(100*COUNT(*)/(SELECT COUNT(*) FROM diary), 2) AS frequencys FROM diary GROUP BY answer4;";
+		$sql = "SELECT answer4, ROUND((100*COUNT(*))/(SELECT COUNT(*) FROM diary WHERE id=".$id."), 2) AS frequencys FROM diary WHERE id=".$id." GROUP BY answer4;";
 		$result = $mysqli->query($sql);
 		echo "<table>";
 		while($row = $result->fetch_assoc()) {
-			echo "<tr>";
-			echo $row["answer4"]. "&nbsp&nbsp&nbsp". $row["frequencys"]."%<br>";
-			echo "<tr>";			
+			if($row["answer4"]==0){
+				echo "<tr>";
+				echo "<td>Jah</td><td>". $row["frequencys"]."%</td>";
+				echo "</tr>";
+			}	
+			if($row["answer4"]==1){
+				echo "<tr>";
+				echo "<td>Ei</td><td>". $row["frequencys"]."%</td>";
+				echo "</tr>";
+			}	
 		}
 		echo "</table>";
 	}
 	if($answerNr4==1){
-		$sql = "SELECT answer6, ROUND(100*COUNT(*)/(SELECT COUNT(*) FROM diary), 2) AS frequencys FROM diary GROUP BY answer6;";
+		$sql = "SELECT answer6, ROUND((100*COUNT(*))/(SELECT COUNT(*) FROM diary WHERE id=".$id."), 2) AS frequencys FROM diary WHERE id=".$id." GROUP BY answer6;";
 		$result = $mysqli->query($sql);
 		echo "<table>";
 		while($row = $result->fetch_assoc()) {
+			echo "<tr>";
 			if($row["answer6"]==NULL){
-				echo "<tr>";
-				echo "Ei mänginud &nbsp&nbsp&nbsp". $row["frequencys"]."%<br>";
-				echo "</tr>";
+				echo "<td>Ei mänginud</td><td>". $row["frequencys"]."%</td>";
 			}else{
-				echo "<tr>";
-				echo $row["answer6"]. "&nbsp&nbsp&nbsp". $row["frequencys"]."%<br>";
-				echo "</tr>";
+				echo "<td>".$row["answer6"]."</td><td>". $row["frequencys"]."%</td>";
 			}
+			echo "</tr>";
 		}
 		echo "</table>";
 	}
