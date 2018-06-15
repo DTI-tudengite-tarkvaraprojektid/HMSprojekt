b60afd73d10d62f3eedf452727b2d7bab8cccac4
Tanel Kuklane <kukltane@greeny.cs.tlu.ee>
2018-06-15 11:53:22 +0300

    answer1.php changes

--- answer1.php
+++ answer1.php
@@ -1,15 +1,17 @@
 <?php
+	$q = $_REQUEST["q"];
+	$v = $_REQUEST["v"];
 	require("../../../../config.php");
 	session_start();
 	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 	$Answers = array();
-	$result = $mysqli->prepare("SELECT date, answer1 FROM diary WHERE id=?;");
+	$result = $mysqli->prepare("SELECT ".$v.", ".$q." FROM diary WHERE id=?;");
 	$result->bind_param("i", $_SESSION["userid"]);
-	$result->bind_result($date, $answer);
+	$result->bind_result($result1, $result2);
 	$result->execute();
 	
 	while($result->fetch()) {
-		array_push($Answers, array($date, $answer));
+		array_push($Answers, array($result1, $result2));
 	}
 	
 	$result->close();
