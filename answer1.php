bafa032f00736704b917f96df6f0dd0456e790dc
Tanel Kuklane <kukltane@greeny.cs.tlu.ee>
2018-06-15 11:06:25 +0300

    answer1.php changes

--- answer1.php
+++ answer1.php
@@ -1,26 +1,19 @@
 <?php
-	$id = $_REQUEST["id"];/*siit edaspidi vaja hoopis oodatava küsimuse välja nime*/
 	require("../../../../config.php");
 	session_start();
 	$mysqli = new mysqli($serverHost, $serverUsername, $serverPassword, $database);
 	$Answers = array();
-	/*$id = $_SESSION["userid"];*/
-	/*$id = 20;*/
 	$result = $mysqli->prepare("SELECT date, answer1 FROM diary WHERE id=?;");
-	$result->bind_param("i", $id);
+	$result->bind_param("i", $_SESSION["userid"]);
 	$result->bind_result($date, $answer);
 	$result->execute();
 	
 	while($result->fetch()) {
 		array_push($Answers, array($date, $answer));
-		/*echo "Sai: ".$date ." ja" .$answer;*/
 	}
 	
 	$result->close();
 	$mysqli->close();
 	
-	/*järgneva asemel json_encode  ja siis see json muutuja echo*/
-	
-	echo "Tulemus on: ";
-	var_dump($Answers);
+	echo json_encode($Answers);
 ?>
